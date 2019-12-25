a3c514b6d3c69dff3edec978f9c81fe5c66b724f
Ajay Paratmandali <ajaypp123@gmail.com>
2019-12-25 22:56:38 +0530

    Solved SSH issue

--- Dummy_System/Node1/setup.sh
+++ Dummy_System/Node1/setup.sh
@@ -3,8 +3,8 @@
 # Dependency
 yum install -y epel-release git vim
 yum install -y python-pip node npm vim*
-yum install https://centos7.iuscommunity.org/ius-release.rpm
-yum install python36u python36u-devel python36u-pip python36-setuptools
+yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+yum install -y python36u python36u-devel python36u-pip python36-setuptools
 python3 easy_install.py pip
 pip3 install –upgrade pip
 
