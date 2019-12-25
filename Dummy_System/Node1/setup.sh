6ecc60f9706ce5d7c1e9a25265ddbf8d38a65fec
Ajay Paratmandali <ajaypp123@gmail.com>
2019-12-25 23:07:04 +0530

    Solved provision problem

--- Dummy_System/Node1/setup.sh
+++ Dummy_System/Node1/setup.sh
@@ -4,8 +4,22 @@
 yum install -y epel-release git vim
 yum install -y python-pip node npm vim*
 yum install -y https://centos7.iuscommunity.org/ius-release.rpm
-yum install -y python36u python36u-devel python36u-pip python36-setuptools
-python3 easy_install.py pip
-pip3 install –upgrade pip
+yum group install "Development Tools"
 
+yum -y install wget
 systemctl stop firewalld
+cp -rf /vagrant/vimrc ~/.vimrc
+
+
+
+	go get -u -v github.com/nsf/gocode
+	go get -u -v github.com/rogpeppe/godef
+	go get -u -v github.com/golang/lint/golint
+	go get -u -v github.com/lukehoban/go-outline
+	go get -u -v sourcegraph.com/sqs/goreturns
+	go get -u -v golang.org/x/tools/cmd/gorename
+	go get -u -v github.com/tpng/gopkgs
+	go get -u -v github.com/newhook/go-symbols
+	go get -u -v golang.org/x/tools/cmd/guru
+	
+ go get -u -v golang.org/x/tools/gopls
