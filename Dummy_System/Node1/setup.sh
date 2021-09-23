8c5a258cbde34a6a44cba3ec4b9c21e81c1d88fb
Ajay Paratmandali <ajaypp123@gmail.com>
2021-09-23 19:29:28 +0530

    Update Dummy setup
    Signed-off-by: Ajay Paratmandali <ajaypp123@gmail.com>

--- Dummy_System/Node1/setup.sh
+++ Dummy_System/Node1/setup.sh
@@ -6,20 +6,24 @@ yum install -y python-pip node npm vim*
 yum install -y https://centos7.iuscommunity.org/ius-release.rpm
 yum group install "Development Tools"
 
+yum install -y gcc rpm-build python36 python36-pip python36-devel python36-setuptools openssl-devel libffi-devel python36-dbus
+
 yum -y install wget
+
 systemctl stop firewalld
+systemctl disable firewalld
 cp -rf /vagrant/vimrc ~/.vimrc
 
 
 
-	go get -u -v github.com/nsf/gocode
-	go get -u -v github.com/rogpeppe/godef
-	go get -u -v github.com/golang/lint/golint
-	go get -u -v github.com/lukehoban/go-outline
-	go get -u -v sourcegraph.com/sqs/goreturns
-	go get -u -v golang.org/x/tools/cmd/gorename
-	go get -u -v github.com/tpng/gopkgs
-	go get -u -v github.com/newhook/go-symbols
-	go get -u -v golang.org/x/tools/cmd/guru
+#	go get -u -v github.com/nsf/gocode
+#	go get -u -v github.com/rogpeppe/godef
+#	go get -u -v github.com/golang/lint/golint
+#	go get -u -v github.com/lukehoban/go-outline
+#	go get -u -v sourcegraph.com/sqs/goreturns
+#	go get -u -v golang.org/x/tools/cmd/gorename
+#	go get -u -v github.com/tpng/gopkgs
+#	go get -u -v github.com/newhook/go-symbols
+#	go get -u -v golang.org/x/tools/cmd/guru
 	
- go get -u -v golang.org/x/tools/gopls
+# go get -u -v golang.org/x/tools/gopls
--- README.md
+++ README.md
@@ -5,4 +5,10 @@ Different example of vagrant.
   It Vagrantfile to give provision for statsd-graphite-Grafana.
   $ git clone url
   $ cd Vagrant_Projects/Stats_Provision
+  $ vagrant plugin install vagrant-vbguest --plugin-version 0.21
   $ vagrant up
+
+
+# Command
+vagrant-cheat-sheet.md from Github gists
+
